0c06a633dec65ce69bd854283d30352a5d22c27e
Debian <admin@ip-172-31-59-208.us-west-2.compute.internal>
2021-08-05 02:15:38 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Requirements
+- docker (requirement for minikube)
+- k8s (minikube used.)
+- kubectl
+- helm
+
+# Steps
+- 01
+- 02
---
+++ script/used/minikube.debian.arm64.20210801.sh
@@ -0,0 +1,2 @@
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_arm64.deb
+sudo dpkg -i minikube_latest_arm64.deb
